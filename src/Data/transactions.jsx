dbf9520f6f659479d86ce56a4f008a9ebe03a822
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-02 23:08:01 +0200

    Add transactions data file
    - Created a new file named `transactions.jsx` in the `Data` directory.
    - Added a `transactions` variable containing transaction data.
    - The `transactions` variable is a list where each item represents a transaction record in the form of an object (dictionary).

---
+++ src/Data/transactions.jsx
@@ -0,0 +1,37 @@
+export const transactions = [
+  {
+    valueType: "+",
+    value: 500,
+    description: "Salary",
+    type: "Income",
+    category: "Work",
+  },
+  {
+    valueType: "-",
+    value: 150,
+    description: "Groceries",
+    type: "Expense",
+    category: "Food",
+  },
+  {
+    valueType: "-",
+    value: 75,
+    description: "Internet Bill",
+    type: "Expense",
+    category: "Utilities",
+  },
+  {
+    valueType: "+",
+    value: 200,
+    description: "Freelance Project",
+    type: "Income",
+    category: "Work",
+  },
+  {
+    valueType: "-",
+    value: 50,
+    description: "Movie Night",
+    type: "Expense",
+    category: "Entertainment",
+  },
+];
