b0fe0c902fb900ed82f95d04d15ef078b5165afe
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-03 23:46:04 +0200

    Add `onClick` handler to "RadioButton" component
    Added an "onClick" handler to the "RadioButton" component to allow custom click behavior. The `onClick` prop is optional.

--- src/Components/RadioButton.jsx
+++ src/Components/RadioButton.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-function RadioButton({ id, name, value, labelValue }) {
+function RadioButton({ id, name, value, labelValue, onClick }) {
   return (
     <>
       <label className="flex cursor-pointer items-center">
@@ -10,6 +10,7 @@ function RadioButton({ id, name, value, labelValue }) {
           name={name}
           value={value}
           className="peer hidden"
+          onClick={onClick}
         />
         <span className="mr-2 inline-block h-4 w-4 rounded-full border border-blue-500 peer-checked:border-transparent peer-checked:bg-blue-500"></span>
         {labelValue}
@@ -23,6 +24,7 @@ RadioButton.propTypes = {
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   labelValue: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default RadioButton;
