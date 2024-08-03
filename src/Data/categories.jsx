a3f6ee1b5ebf76a588ec981b06504f9ed2f475f9
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-03 19:25:25 +0200

    Add file with categories
    Add file "categories.jsx" that has two variables:
    - "expenseCategories" that contains types of expenses
    - "incomeCategories" that contains types of incomes

---
+++ src/Data/categories.jsx
@@ -0,0 +1,18 @@
+export const expenseCategories = [
+  "Food",
+  "Utilities",
+  "Entertainment",
+  "Transportation",
+  "Healthcare",
+  "Education",
+  "Housing",
+  "Gifts",
+];
+
+export const incomeCategories = [
+  "Work",
+  "Freelance",
+  "Savings",
+  "Prizes",
+  "Gifts",
+];
