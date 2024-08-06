79d5584adaf77a637304b68358c806e711a56826
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-06 17:39:56 +0200

    Add dynamic state and UI elements to AddRecordForm
    - Introduce the state management variable `isExpense`;
    - Add two `FormField` components for Amount and Description;
    - Add two `RadioButton` components for Income and Expense;
    - Add a select dropdown with options for categories of Income or Expenses. The displayed options depending on the state of `isExpense`;
    - Create an "Add" button, which will have functionality added in the future;

--- src/Components/AddRecordForm.jsx
+++ src/Components/AddRecordForm.jsx
@@ -1,8 +1,15 @@
 import PropTypes from "prop-types";
+import FormField from "./FormField";
+import RadioButton from "./RadioButton";
+import { useState } from "react";
+import { expenseCategories, incomeCategories } from "../Data/categories";
 
 function AddRecordForm({ onClose }) {
+  const [isExpense, setIsExpense] = useState(true);
+  const optionsToShow = isExpense ? expenseCategories : incomeCategories;
+
   return (
-    <div className="slide-in absolute bottom-0 left-0 h-[40%] w-[100%] bg-cyan-50">
+    <div className="slide-in absolute bottom-0 left-0 h-[40%] w-[100%] bg-cyan-700">
       <div className="w-[100%]bg-blue-500 relative h-[100%]">
         <button
           onClick={onClose}
@@ -10,6 +17,58 @@ function AddRecordForm({ onClose }) {
         >
           ✕
         </button>
+
+        <div className="flex w-[90%] flex-col gap-3 p-5">
+          <FormField
+            id="amount-field"
+            label="Amount"
+            placeholder="95"
+            maxLength={16}
+          />
+          <FormField
+            id="description-field"
+            label="Description"
+            placeholder="Movie Night"
+            maxLength={48}
+          />
+
+          <div className="flex flex-row gap-5">
+            <RadioButton
+              id="expense"
+              name="category"
+              value="-"
+              labelValue="Expense"
+              onClick={() => setIsExpense(true)}
+            />
+            <RadioButton
+              id="income"
+              name="category"
+              value="+"
+              labelValue="Income"
+              onClick={() => setIsExpense(false)}
+            />
+          </div>
+          <div className="flex justify-between">
+            <div className="row flex w-[50%] items-center gap-5">
+              <select
+                id="select-category"
+                className="block w-[50%] rounded-lg border bg-gray-50 px-2.5 py-1.5 text-gray-900"
+              >
+                <option value="" disabled selected hidden>
+                  {isExpense ? "Expense" : "Income"}
+                </option>
+
+                {optionsToShow.map((value, id) => (
+                  <option key={id} value={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+              <label htmlFor="select-category">Category</label>
+            </div>
+            <button className="box-border p-2 pl-5 pr-5 text-sm">Add</button>
+          </div>
+        </div>
       </div>
     </div>
   );
