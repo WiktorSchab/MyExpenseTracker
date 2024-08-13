3fb0b7713b7f9ddb514f547015dedf0e5f847789
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-13 14:46:49 +0200

    Add `ChartComponent.jsx` to render charts
    - Added a `ChartComponent` for rendering customizable charts using Chart.js;
    - Component accepts the following props:
      - `chartType`: Type of the chart (required);
      - `labels`: Array of labels for the x-axis (required);
      - `data`: Array of values for the y-axis (required);
      - `informations`: Object containing:
        - `xLabel`: Label for the x-axis (required);
        - `yLabel`: Label for the y-axis (required);
        - `callbackLabelFun`: Function to format tooltip content (required);
      - `styles`: Object for customizing chart styles, including:
        - `bgColor`: Background color of the chart (optional);
        - `bColor`: Line color (optional);
        - `bWidth`: Line width (optional);
        - `pBgColor`: Point background color (optional);
        - `pBColor`: Point border color (optional);
        - `pBWidth`: Point border width (optional);
        - `pRadius`: Point radius (optional);
    - Added `propTypes` for all props and `defaultProps` for styling;

---
+++ src/Components/ChartComponent.jsx
@@ -0,0 +1,122 @@
+import PropTypes from "prop-types";
+import { Chart as ChartJs } from "chart.js/auto";
+import { useEffect, useRef } from "react";
+
+function ChartComponent({ chartType, labels, data, informations, styles }) {
+  const canvasRef = useRef(null);
+  const chartRef = useRef(null);
+
+  const {
+    bgColor = "bg-blue-200",
+    bColor = "rgb(37, 99, 235)",
+    bWidth = 2,
+    pBgColor = "rgb(37, 99, 235)",
+    pBColor = "rgb(255, 255, 255)",
+    pBWidth = 2,
+    pRadius = 5,
+  } = styles;
+
+  // Informations for chart (labels etc)
+  const { xLabel, yLabel, callbackLabelFun } = informations;
+
+  useEffect(() => {
+    const ctx = canvasRef.current.getContext("2d");
+
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    chartRef.current = new ChartJs(ctx, {
+      type: chartType, // Line chart type
+      data: {
+        labels: labels, // X-axis labels (days of the month)
+        datasets: [
+          {
+            data: data, // Y-axis data (cumulative balance)
+            borderColor: bColor, // Line color
+            borderWidth: bWidth, // Line width
+
+            pointBackgroundColor: pBgColor, // Data points color
+            pointBorderColor: pBColor, // Data points border color
+            pointBorderWidth: pBWidth, // Data points border width
+            pointRadius: pRadius, // Data points radius
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            enabled: true,
+            mode: "index",
+            intersect: false,
+            callbacks: {
+              label: (context) => callbackLabelFun(context),
+            },
+            animation: {
+              duration: 500,
+            },
+            caretSize: 5,
+            caretPadding: 5,
+          },
+        },
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: xLabel,
+            },
+          },
+          y: {
+            title: {
+              display: true,
+              text: yLabel,
+            },
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.destroy();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [labels, data]);
+
+  return (
+    <canvas ref={canvasRef} className={`${bgColor} !h-[300px] p-2`}></canvas>
+  );
+}
+
+ChartComponent.propTypes = {
+  chartType: PropTypes.string.isRequired,
+  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  data: PropTypes.arrayOf(PropTypes.number).isRequired,
+  informations: PropTypes.shape({
+    xLabel: PropTypes.string.isRequired,
+    yLabel: PropTypes.string.isRequired,
+    callbackLabelFun: PropTypes.func.isRequired,
+  }).isRequired,
+  styles: PropTypes.shape({
+    bgColor: PropTypes.string,
+    bColor: PropTypes.string,
+    bWidth: PropTypes.number,
+    pBgColor: PropTypes.string,
+    pBColor: PropTypes.string,
+    pBWidth: PropTypes.number,
+    pRadius: PropTypes.number,
+  }),
+};
+
+ChartComponent.defaultProps = {
+  styles: {},
+};
+
+export default ChartComponent;
