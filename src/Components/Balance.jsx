b86f1326c74d4f9698881b15145b16772d2fe695
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-13 15:12:20 +0200

    Add chart component to `Balance.jsx`
    - Added `useState` hooks to manage labels, balances, and last day balance (monthly balance);
    - Added `callbackLabelFun` function to format tooltips in the chart;
    - Added `useEffect` with:
      - Calculation of cumulative balance for each day;
      - Sorting of days;
    - Added `ChartComponent` to render the chart with prepared data;

--- src/Components/Balance.jsx
+++ src/Components/Balance.jsx
@@ -1,30 +1,76 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 import { transactionShape } from "../Lib/types";
+import ChartComponent from "./ChartComponent"; // Import the Chart component
 
 function Balance({ transactions }) {
-  let sumOfIncome = 0;
-  let sumOfExpense = 0;
+  const [labels, setLabels] = useState([]);
+  const [balances, setBalances] = useState([]);
+  const [lastDayBalance, setLastDayBalance] = useState(0);
 
-  transactions.forEach((transaction) => {
-    if (transaction.valueType === "-") {
-      sumOfExpense += transaction.value;
-    } else {
-      sumOfIncome += transaction.value;
-    }
-  });
+  // Displaying information on data points in chart
+  const callbackLabelFun = (context) => {
+    const day = context.label;
+    const balance = context.raw;
+    return `Day: ${day}, Balance: ${balance}`;
+  };
 
-  const balance = sumOfIncome - sumOfExpense;
+  useEffect(() => {
+    const dailyBalances = {};
+
+    // Getting balance of each day
+    transactions.forEach(({ date, value, valueType }) => {
+      const day = date.slice(8, 10);
+      const adjustedValue = valueType === "-" ? -value : value;
+
+      if (!dailyBalances[day]) {
+        dailyBalances[day] = 0;
+      }
+
+      dailyBalances[day] += adjustedValue;
+    });
+
+    // Sorting days
+    const sortedDays = Object.keys(dailyBalances).sort((a, b) => a - b);
+
+    // Getting cumulative balance of each day
+    const cumulativeBalances = {};
+    let cumulativeBalance = 0;
+    sortedDays.forEach((day) => {
+      cumulativeBalance += dailyBalances[day];
+      cumulativeBalances[day] = cumulativeBalance;
+    });
+
+    // Getting monthly balance
+    const lastDay = sortedDays[sortedDays.length - 1];
+    setLastDayBalance(cumulativeBalances[lastDay] || 0);
+
+    // Convert cumulativeBalances to arrays for charting
+    setLabels(sortedDays);
+    setBalances(sortedDays.map((day) => cumulativeBalances[day]));
+  }, [transactions]);
 
   return (
     <div className="flex flex-col items-center">
-      <h3 className="mb-2 mt-2">Balance: {balance}$</h3>
-      <div className="h-[300px] w-[300px] bg-blue-200"></div>
+      <h3 className="mb-2 mt-2">Balance of the month: {lastDayBalance}$</h3>
+      <div className="border-2 border-slate-50">
+        <ChartComponent
+          chartType="line"
+          labels={labels}
+          data={balances}
+          informations={{
+            xLabel: "Day of the Month",
+            yLabel: "Balance",
+            callbackLabelFun: callbackLabelFun,
+          }}
+        />
+      </div>
     </div>
   );
 }
 
 Balance.propTypes = {
-  transactions: PropTypes.arrayOf(transactionShape),
+  transactions: PropTypes.arrayOf(transactionShape).isRequired,
 };
 
 export default Balance;
