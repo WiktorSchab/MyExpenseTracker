77ad4d9472fab9c3877e0ed8a72af2aba75e9db5
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-14 15:14:33 +0200

    Add settings & download icons to `Header.jsx`
    Added settings & download icons to `Header.jsx`. In future icons will act as buttons that opens settings and download window.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { Cog6ToothIcon, ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 
 function Header({ date, setDate }) {
   const handlePrevMonth = () => {
@@ -23,12 +24,20 @@ function Header({ date, setDate }) {
 
   return (
     <>
-      <header className="flex justify-between">
-        {prevNextButton("←", handlePrevMonth)}
-        <h1 className="mb-2 mt-5">
-          {date.toLocaleString("en-US", { month: "long" })} expenses
-        </h1>
-        {prevNextButton("→", handleNextMonth)}
+      <header className="relative flex justify-center">
+        {/* Settings and download buttons / functionality will be implemented in future */}
+        <div className="absolute left-0 mt-2 flex gap-2">
+          <Cog6ToothIcon className="mr-2 h-6 w-6 cursor-pointer" />
+          <ArrowDownTrayIcon className="mr-2 h-6 w-6 cursor-pointer" />
+        </div>
+
+        <div className="mt-5 flex w-[85%] justify-between">
+          {prevNextButton("←", handlePrevMonth)}
+          <h1 className="mb-2">
+            {date.toLocaleString("en-US", { month: "long" })} expenses
+          </h1>
+          {prevNextButton("→", handleNextMonth)}
+        </div>
       </header>
       <hr />
     </>
