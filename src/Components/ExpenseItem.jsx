16b0eae4b2478a4d462b9fb36447402748019eb3
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-17 15:44:46 +0200

    Refractore styles of `ExpenseItemjsx`
    Refractored styles of `ExpenseItemjsx` to match it with the new size of app

--- src/Components/ExpenseItem.jsx
+++ src/Components/ExpenseItem.jsx
@@ -13,9 +13,9 @@ function ExpenseItem({
 }) {
   return (
     <li className="flex flex-row bg-blue-600 p-2 text-left">
-      <div className="flex w-[500px] justify-between">
+      <div className="flex w-[700px] justify-between">
         <div className="flex flex-row items-center">
-          <p className="w-[150px]">
+          <p className="w-[250px]">
             {valueType}
             {value.toFixed(2)}$ {description}
           </p>
@@ -30,7 +30,7 @@ function ExpenseItem({
           <p>{date}</p>
         </div>
       </div>
-      <div className="ml-auto">
+      <div className="ml-auto flex gap-2">
         <button onClick={() => onEditFunc(id)}>Edit</button>
         <button onClick={() => onDelFunc(id)}>Delete</button>
       </div>
