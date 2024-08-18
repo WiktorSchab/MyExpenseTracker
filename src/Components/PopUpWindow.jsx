18c9af103d50b816e315e6412deafc78a007d1d1
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-18 19:41:01 +0200

    Add `PopUpWindow` component
    Added `PopUpWindow` component that has:
    - Blur effect on the whole app;
    - Displays a single window in the center of the screen with the `children` component inside;
    - Component takes two props:
      - `children`: Component that is displayed inside the window;
      - `closePopUp`: Function to hide the `PopUpWindow`;

---
+++ src/Components/PopUpWindow.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+function PopUpWindow({ children, closePopUp }) {
+  return (
+    <div className="fixed left-0 top-0 z-10 h-full w-full bg-black/40 backdrop-blur">
+      <div className="relative flex h-full w-full items-center justify-center">
+        <button
+          onClick={closePopUp}
+          className="small-round-button text-xxl absolute right-0 top-0 m-5 bg-white text-black"
+        >
+          ✕
+        </button>
+
+        <div className="mb-[50px] flex min-h-[300px] w-[700px] rounded border-2 border-white p-2">
+          <div className="h-full min-h-[300px] w-full rounded bg-blue-400 p-2">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+PopUpWindow.propTypes = {
+  children: PropTypes.element.isRequired,
+  closePopUp: PropTypes.func.isRequired,
+};
+
+export default PopUpWindow;
