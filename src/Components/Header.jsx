8adbe3625f5c898de7684fb891b0ffe38a75ce7f
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-18 20:06:07 +0200

    Add display `Settings`/`DownloaMenu` on click
    Added functionality to `Header.jsx` to display either the `DownloadMenu` or `Settings` component in the `PopUpWindow` component;

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,29 @@
 import PropTypes from "prop-types";
 import { Cog6ToothIcon, ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import PopUpWindow from "./PopUpWindow";
+import Settings from "./Settings";
+import DownloadMenu from "./DownloadMenu";
+import { useState } from "react";
 
 function Header({ date, setDate }) {
+  const [isPopUpDisplayed, setIsPopUpDisplayed] = useState(false);
+  const [popUpComp, setPopUpComp] = useState(null);
+
+  const generatePopUp = (popUpComp) => {
+    return <PopUpWindow closePopUp={closePopUp}>{popUpComp}</PopUpWindow>;
+  };
+
+  const closePopUp = () => setIsPopUpDisplayed(false);
+
+  const handleSettingsOpen = () => {
+    setPopUpComp(<Settings />);
+    setIsPopUpDisplayed(true);
+  };
+  const handleDownloadMenuOpen = () => {
+    setPopUpComp(<DownloadMenu />);
+    setIsPopUpDisplayed(true);
+  };
+
   const handlePrevMonth = () => {
     const newDate = new Date(date);
     newDate.setMonth(date.getMonth() - 1);
@@ -25,12 +47,18 @@ function Header({ date, setDate }) {
   return (
     <>
       <header className="relative flex justify-center">
+        {isPopUpDisplayed && generatePopUp(popUpComp)}
         {/* Settings and download buttons / functionality will be implemented in future */}
         <div className="absolute left-0 mt-2 flex gap-2">
-          <Cog6ToothIcon className="mr-2 h-6 w-6 cursor-pointer" />
-          <ArrowDownTrayIcon className="mr-2 h-6 w-6 cursor-pointer" />
+          <Cog6ToothIcon
+            className="mr-2 h-6 w-6 cursor-pointer"
+            onClick={handleSettingsOpen}
+          />
+          <ArrowDownTrayIcon
+            className="mr-2 h-6 w-6 cursor-pointer"
+            onClick={handleDownloadMenuOpen}
+          />
         </div>
-
         <div className="mt-5 flex w-[85%] justify-between">
           {prevNextButton("←", handlePrevMonth)}
           <h1 className="mb-2">
