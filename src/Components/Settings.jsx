d70141b9a793529ee6b94c2728dd140fb51a4b1e
WiktorSchab <wiktormateuszschab@gmail.com>
2024-08-19 16:46:42 +0200

    Add function to download transaction file
    Added function to download `localStorage` `transaction` object as file in `json` extension. If `transaction` variable is empty or was not found error will be displayed above the button;

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,4 +1,4 @@
-import { Children } from "react";
+import { Children, useState } from "react";
 import PropTypes from "prop-types";
 
 // Component to generate list of option buttons in settings
@@ -20,16 +20,52 @@ GenerateButtonList.propTypes = {
 };
 
 function Settings() {
+  const [downloadDataError, setDownloadDataError] = useState(null);
+
+  const downloadTransaction = () => {
+    const transactions = localStorage.getItem("transactions");
+
+    if (!transactions) {
+      setDownloadDataError("No transactions found in localStorage.");
+      return;
+    } else {
+      setDownloadDataError();
+    }
+
+    const blob = new Blob([transactions], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "transactions.json";
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <>
       <h5 className="mb-5">Settings</h5>
       <div className="flex w-[100%] justify-between px-5">
         <GenerateButtonList>
-          <button className="mb-2 w-full" onClick={deleteTransaction}>
-            Delete transactions data
-          </button>
-          <button className="mb-2 w-full">Download transactions data</button>
-          <button className="mb-2 w-full">Upload transactions data</button>
+          <div>
+            <button className="mb-2 w-full" onClick={deleteTransaction}>
+              Delete transactions data
+            </button>
+          </div>
+          <div>
+            {downloadDataError && (
+              <span className="block text-center font-bold text-red-400">
+                {downloadDataError}
+              </span>
+            )}
+            <button className="mb-2 w-full" onClick={downloadTransaction}>
+              Download transactions data
+            </button>
+          </div>
+          <div>
+            <button className="mb-2 w-full">Upload transactions data</button>
+          </div>
         </GenerateButtonList>
         <GenerateButtonList>
           <button className="w-full">Night mode ON/Day mode ON</button>
